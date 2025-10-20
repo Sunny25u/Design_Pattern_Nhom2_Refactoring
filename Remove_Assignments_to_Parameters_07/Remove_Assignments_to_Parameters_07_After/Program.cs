8a4abc5b77b5a5282dd10f8fb02ea3d37f844e03
huutai2003 <69775996+huutai2003@users.noreply.github.com>
2025-10-20 09:53:30 +0700

    Add files via upload

---
+++ Remove_Assignments_to_Parameters_07/Remove_Assignments_to_Parameters_07_After/Program.cs
@@ -0,0 +1,15 @@
+﻿int Discount(int inputVal, int quantity)
+{
+    int result = inputVal; // tạo biến tạm để thao tác
+
+    if (quantity > 100)
+    {
+        result -= 5;
+    }
+    else if (quantity > 50)
+    {
+        result -= 2;
+    }
+
+    return result;
+}
---
+++ Remove_Assignments_to_Parameters_07/Remove_Assignments_to_Parameters_07_Before/Program.cs
@@ -0,0 +1,13 @@
+﻿int Discount(int inputVal, int quantity)
+{
+    if (quantity > 50)
+    {
+        inputVal -= 2;
+    }
+    else if (quantity > 100)
+    {
+        inputVal -= 5;
+    }
+
+    return inputVal;
+}
