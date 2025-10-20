dc135bba65c13baf498820a053e5958b8938e43f
Khoaschool09 <khoa_dpm225434@student.agu.edu.vn>
2025-10-20 08:53:39 +0700

    Add files via upload

---
+++ After_ReplaceTypeCodewithSubclasses_30/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    abstract class Employee
+    {
+        protected string name;
+
+        protected Employee(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double GetPayAmount();
+
+        public override string ToString() => $"{name} ({GetType().Name})";
+
+        // Factory method: tạo Employee phù hợp với type code cũ
+        public static Employee Create(string name, int type)
+        {
+            return type switch
+            {
+                0 => new Engineer(name),
+                1 => new Salesman(name),
+                _ => throw new ArgumentException("Invalid employee type")
+            };
+        }
+    }
+
+    class Engineer : Employee
+    {
+        public Engineer(string name) : base(name) { }
+
+        public override double GetPayAmount() => 5000.0;
+    }
+
+    class Salesman : Employee
+    {
+        public Salesman(string name) : base(name) { }
+
+        public override double GetPayAmount() => 3000.0 + GetCommission();
+
+        private double GetCommission() => 500.0;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee e1 = Employee.Create("An", 0);
+            Employee e2 = Employee.Create("Bình", 1);
+
+            Console.WriteLine($"{e1}: {e1.GetPayAmount()}$");
+            Console.WriteLine($"{e2}: {e2.GetPayAmount()}$");
+        }
+    }
+}
---
+++ Before_ReplaceTypeCodewithSubclasses_30/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    class Employee
+    {
+        public static readonly int ENGINEER = 0;
+        public static readonly int SALESMAN = 1;
+
+        private string name;
+        private int type;
+
+        public Employee(string name, int type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+
+        public int GetTypeCode() => type;
+
+        public double GetPayAmount()
+        {
+            switch (type)
+            {
+                case ENGINEER:
+                    return 5000.0;
+                case SALESMAN:
+                    return 3000.0 + GetCommission();
+                default:
+                    throw new Exception("Invalid employee type");
+            }
+        }
+
+        private double GetCommission() => 500.0;
+
+        public override string ToString() => $"{name} ({GetRoleName()})";
+
+        private string GetRoleName()
+        {
+            return type == ENGINEER ? "Engineer" : "Salesman";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee e1 = new Employee("An", Employee.ENGINEER);
+            Employee e2 = new Employee("Bình", Employee.SALESMAN);
+
+            Console.WriteLine($"{e1}: {e1.GetPayAmount()}$");
+            Console.WriteLine($"{e2}: {e2.GetPayAmount()}$");
+        }
+    }
+}
