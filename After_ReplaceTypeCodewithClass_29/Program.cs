ea03b79dc3c9520d07341d5087d0d65e10c5470d
Khoaschool09 <khoa_dpm225434@student.agu.edu.vn>
2025-10-20 08:51:04 +0700

    Add files via upload

---
+++ After_ReplaceTypeCodewithClass_29/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    class BloodGroup
+    {
+        public static readonly BloodGroup O = new BloodGroup("O");
+        public static readonly BloodGroup A = new BloodGroup("A");
+        public static readonly BloodGroup B = new BloodGroup("B");
+        public static readonly BloodGroup AB = new BloodGroup("AB");
+
+        public string Name { get; }
+
+        private BloodGroup(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+    }
+
+    class Person
+    {
+        private string name;
+        private BloodGroup bloodGroup;
+
+        public Person(string name, BloodGroup bloodGroup)
+        {
+            this.name = name;
+            this.bloodGroup = bloodGroup;
+        }
+
+        public BloodGroup GetBloodGroup() => bloodGroup;
+
+        public override string ToString() => $"{name} - Nhóm máu {bloodGroup}";
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person p = new Person("An", BloodGroup.A);
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ Before_ReplaceTypeCodewithClass_29/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    class Person
+    {
+        public static readonly int O = 0;
+        public static readonly int A = 1;
+        public static readonly int B = 2;
+        public static readonly int AB = 3;
+
+        private string name;
+        private int bloodGroup;  // ❌ Type code: chỉ là số nguyên
+
+        public Person(string name, int bloodGroup)
+        {
+            this.name = name;
+            this.bloodGroup = bloodGroup;
+        }
+
+        public int GetBloodGroup() => bloodGroup;
+
+        public override string ToString()
+        {
+            string[] groups = { "O", "A", "B", "AB" };
+            return $"{name} - Nhóm máu {groups[bloodGroup]}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person p = new Person("An", Person.A);
+            Console.WriteLine(p);
+        }
+    }
+}
