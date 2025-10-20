253f120414c9cc485e64212868e01b331d6811ee
Khoaschool09 <khoa_dpm225434@student.agu.edu.vn>
2025-10-20 08:45:09 +0700

    Add files via upload

---
+++ After_ReplaceMagicNumberwithSymbolicConstant_26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    class Program
+    {
+        // Thay số 9.81 bằng một hằng số có tên rõ ràng
+        const double GRAVITATIONAL_CONSTANT = 9.81;
+
+        static double PotentialEnergy(double mass, double height)
+        {
+            return mass * height * GRAVITATIONAL_CONSTANT;
+        }
+
+        static void Main(string[] args)
+        {
+            double m = 10;  // kg
+            double h = 5;   // m
+            double energy = PotentialEnergy(m, h);
+
+            Console.WriteLine($"Thế năng là: {energy} Joules");
+        }
+    }
+}
---
+++ Before_ReplaceMagicNumberwithSymbolicConstant_26/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+    class Program
+    {
+        static double PotentialEnergy(double mass, double height)
+        {
+            // 9.81 là số ma thuật (acceleration due to gravity)
+            return mass * height * 9.81;
+        }
+
+        static void Main(string[] args)
+        {
+            double m = 10;     // khối lượng (kg)
+            double h = 5;      // độ cao (m)
+            double energy = PotentialEnergy(m, h);
+
+            Console.WriteLine($"Thế năng là: {energy} Joules");
+        }
+    }
+}
