6ea9eaa0c75c2f6417c073a52570c18af9ad3b39
duydpm225407-art <duy_dpm225407@student.agu.edu.vn>
2025-11-03 07:29:16 +0700

    Add files via upload

---
+++ Before_ReplaceSubclassWithFields_31/Before_ReplaceSubclassWithFields_31/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem_TypeCodeWithState
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee(Employee.ENGINEER);
+            Console.WriteLine("Bonus as Engineer: " + employee.GetBonus());
+
+            employee.Type = Employee.MANAGER; // Thay đổi loại khi đang chạy
+            Console.WriteLine("Bonus as Manager: " + employee.GetBonus());
+        }
+    }
+    class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int MANAGER = 1;
+        public int Type { get; set; }
+        public Employee(int type) { Type = type; }
+        public double GetBonus()
+        {
+            switch (Type)
+            {
+                case ENGINEER: return 100;
+                case MANAGER: return 200;
+                default: return 0;
+            }
+        }
+    }
+}
